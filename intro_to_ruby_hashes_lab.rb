b021a6a3ee158f157d905277add4bfe28c218401
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 22:53:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {
+  my_hash_creator = {
        key=>value
   }
   #my_hash_creator('name''value')
