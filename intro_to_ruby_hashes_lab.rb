902e3e0a1de2b3310bb2b4201989e920f6d865fd
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 23:09:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   #read_from_hash
-  hash = {
-
-  }
+  hash = "Steve"
 end
 
 def update_counting_hash(hash, key)
