5a076481e5493e67a749bedc9d402395b6c5d8cd
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 21:42:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
   # return an empty hash
 {
-  
+
 }
 end
 
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  {
+    name:"solomon"
+  }
 end
 
 def pioneer
