a90a1bed3be83c7c6e1a264eb19dca541e9da514
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 02:22:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ end
   # if the provided key is present, increment its value by 1
 
 
-def update_counting_hash(hash, key
+def update_counting_hash(hash, key)
 #if hash[:hello]
 if hash[key]
   #hash[key]= 1
